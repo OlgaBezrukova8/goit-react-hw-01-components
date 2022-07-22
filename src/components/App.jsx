613a71c13dc27a655826858ad65bad10ba66a5b1
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-22 18:36:18 +0100

    Added Statistics component with propTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import Profile from 'components/Profile/Profile';
 import FriendList from 'components/FriendList/FriendList';
 import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import Statistics from 'components/Statistics/Statistics';
 
 import user from 'components/Profile/user.json';
 import friends from 'components/FriendList/friends.json';
 import transactions from 'components/TransactionHistory/transactions.json';
+import data from 'components/Statistics/data.json';
 
 export const App = () => {
   return (
-    (
+    <div>
       <Profile
         name={user.username}
         tag={user.tag}
@@ -20,8 +22,10 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-    ),
-    (<FriendList friends={friends} />),
-    (<TransactionHistory items={transactions} />)
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </div>
   );
 };
