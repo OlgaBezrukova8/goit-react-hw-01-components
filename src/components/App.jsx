65cdbf35718386f39a7e44dad9588290eca639d8
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-22 19:41:55 +0100

    Added css styles for Profile component and general styles in project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
 import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
-import Statistics from 'components/Statistics/Statistics';
 
 import user from 'components/Profile/user.json';
+import data from 'components/Statistics/data.json';
 import friends from 'components/FriendList/friends.json';
 import transactions from 'components/TransactionHistory/transactions.json';
-import data from 'components/Statistics/data.json';
 
 export const App = () => {
   return (
     <div>
       <Profile
-        name={user.username}
+        username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
@@ -22,10 +22,10 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
     </div>
   );
 };
