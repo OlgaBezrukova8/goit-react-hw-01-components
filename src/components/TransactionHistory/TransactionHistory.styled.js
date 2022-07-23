79b82b165b3d3178c5a3f1ffd5650ad64b920de7
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-23 16:44:10 +0100

    Added changes in TransactionHistory styles

--- src/components/TransactionHistory/TransactionHistory.styled.js
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -7,14 +7,25 @@ export const Section = styled.section`
 
 export const Table = styled.table`
   margin: 0 auto;
+
   border-collapse: collapse;
   border: 2px solid var(--primary-white-color);
+  box-shadow: 0px 0px 8px 0px rgba(34, 60, 80, 0.2);
 `;
 
 export const TableName = styled.tr`
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+
   background-color: var(--table-background-color);
 `;
 
-export const TableValue = styled.tr``;
+export const TableValue = styled.tr`
+  font-weight: 400;
+  border: 1px solid var(--primary-white-color);
 
-
+  &:nth-child(2n) {
+    background-color: var(--table-color);
+  }
+`;
