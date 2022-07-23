b4a25380bae4b6f1ed9591c665ae4f571fa71eaa
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-23 12:04:03 +0100

    Added changes in Statistics component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -19,6 +19,7 @@ export default function Statistics({ title, stats }) {
 }
 
 Statistics.propTypes = {
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
