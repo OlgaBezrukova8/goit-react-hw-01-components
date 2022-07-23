8088a477adc9968627e38cd1cf277661c833fd18
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-23 11:50:11 +0100

    Added changes with .json files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,10 @@ import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
 import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
-import user from 'components/Profile/user.json';
-import data from 'components/Statistics/data.json';
-import friends from 'components/FriendList/friends.json';
-import transactions from 'components/TransactionHistory/transactions.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
