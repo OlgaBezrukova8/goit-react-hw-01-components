c10e0a47602ea6903d264494a673bf032901acd7
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-23 16:12:14 +0100

    Added changes in TransactionHistory styles

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Table,
+  Section,
+  TableName,
+  TableValue,
+} from 'components/TransactionHistory/TransactionHistory.styled';
 
 export default function TransactionHistory({ items }) {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
+    <Section>
+      <Table className="transaction-history">
+        <thead>
+          <TableName>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </TableName>
+        </thead>
 
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <TableValue key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </TableValue>
+          ))}
+        </tbody>
+      </Table>
+    </Section>
   );
 }
 
---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  padding: 60px 0px 60px 0px;
+  background-color: var(--card-background-color);
+`;
+
+export const Table = styled.table`
+  margin: 0 auto;
+  border-collapse: collapse;
+  border: 2px solid var(--primary-white-color);
+`;
+
+export const TableName = styled.tr`
+  background-color: var(--table-background-color);
+`;
+
+export const TableValue = styled.tr``;
+
+
